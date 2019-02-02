e5a0bca3c06645daf10449e3b1826ee9a4217a6a
Zhuoia <zhuoia@163.com>
2019-02-03 00:23:59 +0800

    how to use abstract class and interface to implement a BST

---
+++ java/DataStructure/BST-1/AbstractTree.java
@@ -0,0 +1,12 @@
+public  abstract class AbstractTree<E> implements Tree<E> {
+    @Override
+    public void inorder() {}
+    
+    @Override
+    public void postorder() {}
+    
+    @Override
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+}
