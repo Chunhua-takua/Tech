9de5cfa060d1941d6fb5ac33060236ea8b68a760
Zhuoia <zhuoia@163.com>
2019-02-16 01:08:21 +0800

    ???

--- java/DataStructure/BST/BSTDemo.java
+++ java/DataStructure/BST/BSTDemo.java
@@ -1,10 +1,17 @@
 public class BSTDemo {
+   // TreeNode<Integer> root;
+    
     public static void main(String[] args) {
         TreeNode<Integer> root = new TreeNode<>(60);
         root.left = new TreeNode<>(55);
         root.right = new TreeNode<>(100);
         
-        // System.out.println(root.e)
-        root.printNode();
+        System.out.println("60, ", TreeNode.search(root, 60));
+        System.out.println("100, ", TreeNode.search(root, 100));
+        System.out.println("55, ", TreeNode.search(root, 55));
+        System.out.println("61, ", TreeNode.search(root, 61));
+        System.out.println("101, ", TreeNode.search(root, 101));
     }
+    
+
 }
