318505fac1251989ec3d907aa4cbb97d605fb3cd
Zhuoia <zhuoia@163.com>
2019-02-24 21:53:24 +0800

    Learn how to use javascript

---
+++ javascript/nowcoder/sum.js
@@ -0,0 +1,21 @@
+/*
+* 题目描述
+* 计算给定数组 arr 中所有元素的总和
+* 输入描述:
+* 数组中的元素均为 Number 类型
+* 示例1
+* 输入
+* 
+* [ 1, 2, 3, 4 ]
+* 输出
+* 
+* 10
+*/
+
+function sum(arr) {
+	var sum =  0;
+	for (var i = 0; i < arr.length; i++)
+		sum += arr[i];
+	
+	return sum;
+}
