82ada08422a9de963b4aa02a2a7379f67a05bef3
Zhuoia <zhuoia@163.com>
2019-05-01 15:48:00 +0800

    Story: 单例模式-基本模式，e.g., TaskManger

---
+++ DesignPattern/SingletonPattern/TaskManager.java
@@ -0,0 +1,28 @@
+/**
+ * Singleton pattern
+ * 一个类只有一个实例，且自行实例化并向整个系统提供这个实例。这个类称为单例类，提供全局访问方法
+ * 单例模式示例
+ * OS中的资源管理器不论启动多少次都只显示一个窗口
+ */
+
+class TaskManager {
+	// static 保证系统中只有一个实例
+    private static TaskManager tm = null;
+
+    // 构造方法为private，保证外部无法调用构造方法新建实例
+    private TaskManager {
+    }
+
+	// public 保证外部可以得到实例
+	// static 保证不需要实例化这个类就可以调用实例
+    public static getInstance() {
+        if (tm == null)
+            tm = new TaskManager();
+        return tm;
+    }
+	
+	// 其他方法
+	public void displayProcesses() {}
+	
+	public void displayServices() {}
+}
