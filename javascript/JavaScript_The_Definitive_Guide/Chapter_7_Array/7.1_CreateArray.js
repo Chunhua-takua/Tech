7ef057041a36b12ec64aad664972caa80d2624c4
Zhuoia <zhuoia@163.com>
2019-05-11 17:27:42 +0800

    JavaScript, chapter 7, 7.1

---
+++ javascript/JavaScript_The_Definitive_Guide/Chapter_7_Array/7.1_CreateArray.js
@@ -0,0 +1,7 @@
+var empty = []; // With no element
+var primes = [2, 3, 5, 7, 11]; // Having five elements
+var misc = [1.1, true, "a", ]; // Element of different types
+
+// 数组中可以使用表达式
+var base = 1024;
+var table = [base, base + 1, base + 2, base + 3];
