e95d6312d3bf064b7633493296d7e482639c40d1
ThomsonTak <1050219236@qq.com>
2019-06-26 21:00:43 +0800

    node.nodeType

--- javascript/JavaScript_Dom/Chapter_4/business/actions.js
+++ javascript/JavaScript_Dom/Chapter_4/business/actions.js
@@ -1,16 +1,18 @@
-
-
 function showPic(whichPic) {
+    debugger;
     var source = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
 
+    var text = whichPic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.childNodes[0].nodeValue = text;
 }
 
 function countBodyChildren() {
     // Get elements named 'body'
     var body_element = document.getElementsByTagName("body")[0];
-    alert (body_element.nodeType);
+    // alert (body_element.nodeType);
 }
 
 // Execute function countBodyChildren when the page is loaded
