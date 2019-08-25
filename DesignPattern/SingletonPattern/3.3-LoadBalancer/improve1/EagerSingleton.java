6cd439b12be59d87c2b84f38540dcc8e6025fcea
Zhuoia <zhuoia@163.com>
2019-08-25 18:49:53 +0800

    eager singleton

---
+++ DesignPattern/SingletonPattern/3.3-LoadBalancer/improve1/EagerSingleton.java
@@ -0,0 +1,8 @@
+class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+    private EagerSingleton() {}
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
