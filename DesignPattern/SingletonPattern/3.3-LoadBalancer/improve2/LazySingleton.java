d5ff1f20e926e66f8d0218efb2bdef73bd5de8eb
Zhuoia <zhuoia@163.com>
2019-09-01 21:53:18 +0800

    comments

---
+++ DesignPattern/SingletonPattern/3.3-LoadBalancer/improve2/LazySingleton.java
@@ -0,0 +1,15 @@
+class LazySingleton {
+    /**
+     * 使用synchronized解决线程安全问题
+     * 类加载时不实例化，而是调用getInstance时，称为延迟加载技术
+     * 在高并发情形下性能比较低
+     */
+    private static LazySingleton instance = null;
+
+    public synchronized LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
